148e0e37c16b58d28df6ef3066a5da12c37a2098
Somvan <89217061+SomVanTeam@users.noreply.github.com>
2024-08-21 23:37:30 +0300

    Update arx.js

--- arx.js
+++ arx.js
@@ -16,20 +16,21 @@ var currentURL = window.location.href;
 var newerURL = currentURL.replace('https://linkvertise.com/376138/arceus-x-neo-key-system-1?o=sharing', 'https://lootdest.com/s?qljL');
 var pageTitle = document.title;
 var arceusapi = "https://spdmteam.com/api/keysystem?step=";
+var arceusapisuffix = "&advertiser=linkvertise&OS=ios";
 if (currentURL.includes("https://linkvertise.com/376138/arceus-x-neo-key-system-1?o=sharing")) {
     window.location.replace(newerURL);
 } else if (pageTitle.includes("NEO") && pageTitle.includes("1")) {
-    alert("checkpoint1test");
-    window.location.href = arceusapi + "1&advertiser=linkvertise&OS=ios";
+    //alert("checkpoint1test");
+    window.location.href = arceusapi + "1" + arceusapisuffix;
 } else if (currentURL.includes("https://spdmteam.com/key-system-2?hwid=")) {
     window.location.replace("https://loot-link.com/s?mYit");
 } else if (pageTitle.includes("NEO") && pageTitle.includes("2")) {
-    window.location.replace(arceusapi + "2&advertiser=linkvertise&OS=ios");
+    window.location.replace(arceusapi + "2" + arceusapisuffix);
 } else if (currentURL.includes("https://spdmteam.com/key-system-3?hwid=")) {
     window.location.replace("https://loot-link.com/s?qlbU");
 } else if (pageTitle.includes("NEO") && pageTitle.includes("3")) {
-    window.location.replace(arceusapi + "3&advertiser=linkvertise&OS=ios");
+    window.location.replace(arceusapi + "3" + arceusapisuffix);
 } else if (currentURL.includes("/key-system-getkey")) {
-    alert("Whitelisted successfully!")
+    //alert("Whitelisted successfully!")
 }
-console.log("Malware bypasser loaded");
+//console.log("Malware bypasser loaded");

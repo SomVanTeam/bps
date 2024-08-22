7212236dfab24be15cf41567cf2954dbe3eef247
Somvan <89217061+SomVanTeam@users.noreply.github.com>
2024-08-22 22:27:21 +0300

    Update d.js

--- usv1/d.js
+++ usv1/d.js
@@ -2,26 +2,13 @@ var s_e = true
 if (document.title == 'Just a moment...') {
     return;
 }
-
-function notif(message, timeout) {
-    const config = {
-        text: message,
-        title: "VYPASS",
-        silent: true,
-    };
-    if (timeout) config.timeout = timeout*1000;
-    GM_notification(config);
-}
-
 if (s_e!=true) {
     return;
 }
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-var currentURL = window.location.href;
 let alpha = new URL(window.location.href).searchParams.get("id"), beta = await (await fetch("https://api-gateway.platoboost.com/v1/authenticators/8/" + alpha)).json();
 if (beta.key) return;
 let gamma = new URL(window.location.href).searchParams.get("tk");
-if (gamma) await sleep(3e3), await (await fetch(`https://api-gateway.platoboost.com/v1/sessions/auth/8/${alpha}/${gamma}`, {
+if (gamma) await gmsleepdecl(3e3), await (await fetch(`https://api-gateway.platoboost.com/v1/sessions/auth/8/${alpha}/${gamma}`, {
     method: "PUT"
 })).json().then(async alpha => {
     window.location.assign(alpha.redirect)
@@ -40,7 +27,7 @@ else {
                 type: epsilon ? "Turnstile" : ""
             })
          });
-    zeta = await zeta.json(), await sleep(1e3);
+    zeta = await zeta.json(), await gmsleepdecl(1e3);
     let eta = await (await fetch(`https://bypass.rblx.workers.dev/delta-decrypt?url=${encodeURIComponent(n.redirect)}`)).text(),
         theta = new URL(eta).searchParams.get("r"),
         iota = atob(theta);

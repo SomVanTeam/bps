bd007100485cb0f9f27f7771fda32ac13a2fd18e
Somvan <89217061+SomVanTeam@users.noreply.github.com>
2024-08-22 17:48:15 +0300

    Update d.js

--- usv1/d.js
+++ usv1/d.js
@@ -1,1 +1,18 @@
+var s_e = true
+if (document.title == 'Just a moment...') {
+    return;
+}
 
+function notif(message, timeout) {
+    const config = {
+        text: message,
+        title: "VYPASS",
+        silent: true,
+    };
+    if (timeout) config.timeout = timeout;
+    GM_notification(config);
+}
+
+if (s_e!=true) {
+    return;
+}

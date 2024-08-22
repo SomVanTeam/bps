58dddeddb6ed1597f290e26908e92c5a4bf67244
Somvan <89217061+SomVanTeam@users.noreply.github.com>
2024-08-22 22:26:08 +0300

    Update a.js

--- usv1/a.js
+++ usv1/a.js
@@ -5,24 +5,24 @@ if (document.title == 'Just a moment...') {
 if (s_e!=true) {
     return;
 }
-var newerURL = currentURL.replace('https://linkvertise.com/376138/arceus-x-neo-key-system-1?o=sharing', 'https://lootdest.com/s?qljL');
+var newerURL = gmcurrentURLdecl.replace('https://linkvertise.com/376138/arceus-x-neo-key-system-1?o=sharing', 'https://lootdest.com/s?qljL');
 var pageTitle = document.title;
 var arceusapi = "https://spdmteam.com/api/keysystem?step=";
 var arceusapisuffix = "&advertiser=linkvertise&OS=ios";
-if (currentURL.includes("https://linkvertise.com/376138/arceus-x-neo-key-system-1?o=sharing")) {
+if (gmcurrentURLdecl.includes("https://linkvertise.com/376138/arceus-x-neo-key-system-1?o=sharing")) {
     window.location.replace(newerURL);
 } else if (pageTitle.includes("NEO") && pageTitle.includes("1")) {
     //alert("checkpoint1test");
     window.location.href = arceusapi + "1" + arceusapisuffix;
-} else if (currentURL.includes("https://spdmteam.com/key-system-2?hwid=")) {
+} else if (gmcurrentURLdecl.includes("https://spdmteam.com/key-system-2?hwid=")) {
     window.location.replace("https://loot-link.com/s?mYit");
 } else if (pageTitle.includes("NEO") && pageTitle.includes("2")) {
     window.location.replace(arceusapi + "2" + arceusapisuffix);
-} else if (currentURL.includes("https://spdmteam.com/key-system-3?hwid=")) {
+} else if (gmcurrentURLdecl.includes("https://spdmteam.com/key-system-3?hwid=")) {
     window.location.replace("https://loot-link.com/s?qlbU");
 } else if (pageTitle.includes("NEO") && pageTitle.includes("3")) {
     window.location.replace(arceusapi + "3" + arceusapisuffix);
-} else if (currentURL.includes("/key-system-getkey")) {
+} else if (gmcurrentURLdecl.includes("/key-system-getkey")) {
     //alert("Whitelisted successfully!")
 }
 //console.log("Malware bypasser loaded");

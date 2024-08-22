55fc6a39eebf4c2cfc01293cf91d1bae57e0b9ee
Somvan <89217061+SomVanTeam@users.noreply.github.com>
2024-08-22 22:23:23 +0300

    Update decl.js

--- usv1/decl.js
+++ usv1/decl.js
@@ -1,1 +1,9 @@
-
+const function gmnotifaab(message, timeout) {
+    const config = {
+        text: message,
+        title: "VYPASS",
+        silent: true,
+    };
+    if (timeout) config.timeout = timeout*1000;
+    GM_notification(config);
+}

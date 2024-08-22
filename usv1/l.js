476eba13ede617b47d71cca943445d2cf64bb9d8
Somvan <89217061+SomVanTeam@users.noreply.github.com>
2024-08-22 21:56:17 +0300

    Update l.js

--- usv1/l.js
+++ usv1/l.js
@@ -1,1 +1,12 @@
 
+function linkvrts(link) {
+  return new Promise((resolve, reject) => {
+    GM.xmlHttpRequest({
+      method: "GET",
+      url: link,
+      headers: { Referer: 'https://linkvertise.com/' },
+      onload: response => resolve(response.responseText),
+      onerror: error => reject(error)
+    });
+  });
+}

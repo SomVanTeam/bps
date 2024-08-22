123ab4deb0a826145050d5b91a5929e0c5f3cfa8
Somvan <89217061+SomVanTeam@users.noreply.github.com>
2024-08-22 22:24:39 +0300

    Update decl.js

--- usv1/decl.js
+++ usv1/decl.js
@@ -1,4 +1,4 @@
-const function gmnotifaab(message, timeout) {
+const function gmnotifdecl(message, timeout) {
     const config = {
         text: message,
         title: "VYPASS",
@@ -7,3 +7,5 @@ const function gmnotifaab(message, timeout) {
     if (timeout) config.timeout = timeout*1000;
     GM_notification(config);
 }
+const gmsleepdecl = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const gmcurrentURLdecl = window.location.href;
